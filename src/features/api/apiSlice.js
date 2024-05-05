e01d3a4919efeaff2dc9d0b4ed203301eb06db59
vinothsmart <s.vinothkanna90@gmail.com>
2024-05-05 13:00:32 +0530

    add todo working

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,9 +3,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500" }),
+  tagTypes: ["Todos"],
   endpoints: (builder) => ({
     getTodos: builder.query({
       query: () => "todos",
+      providesTags: ["Todos"],
     }),
     addTodo: builder.mutation({
       query: (body) => ({
@@ -13,6 +15,7 @@ export const apiSlice = createApi({
         method: "POST",
         body,
       }),
+      invalidatesTags: ["Todos"],
     }),
     updateTodo: builder.mutation({
       query: ({ id, body }) => ({
@@ -20,6 +23,7 @@ export const apiSlice = createApi({
         method: "PATCH",
         body,
       }),
+      invalidatesTags: ["Todos"],
     }),
     deleteTodo: builder.mutation({
       query: ({ id }) => ({
@@ -27,6 +31,7 @@ export const apiSlice = createApi({
         method: "DELETE",
         body: id,
       }),
+      invalidatesTags: ["Todos"],
     }),
   }),
 });
